0d6c6311d943aa00e296d3955f929d137abd85e9
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-12-02 14:45:27 +0100

    Write genres to file in out dir

--- src/genres.py
+++ src/genres.py
@@ -1,5 +1,23 @@
 from artists import artists_in_playlist, unique_artist_ids, all_artist_ids
 from utils.dict_utils import sort_dict_desc
+from pathlib import Path
+import json
+
+
+def write_genres(genre_dict_list):
+    Path("../out").mkdir(parents=True, exist_ok=True)
+    with open('../out/genres.json', 'w') as convert_file:
+        convert_file.write(json.dumps(genre_dict_list, indent=2))
+
+
+def save_genres(total_genres, unique_artist_genres):
+    sorted_genres_all = sort_dict_desc(total_genres)
+    sorted_genres_unique = sort_dict_desc(unique_artist_genres)
+
+    sorted_genres_all = {"name": "All artists", **sorted_genres_all}
+    sorted_genres_unique = {"name": "Unique artists", **sorted_genres_unique}
+
+    write_genres([sorted_genres_all, sorted_genres_unique])
 
 
 def genres_for_artist(sp, artist_id):
@@ -38,7 +56,9 @@ def genres(sp, playlist_id):
     genres_for_all_artists = genres_from_artist_list(sp, all_artists)
     genres_for_unique_artists = genres_from_artist_list(sp, unique_artists)
 
-    return genres_for_unique_artists
+    save_genres(genres_for_all_artists, genres_for_unique_artists)
+
+    return genres_for_all_artists
 
 
 def genre_stats(sp, playlist_id):
